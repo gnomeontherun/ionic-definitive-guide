b8e5d8fab19a59949ad3ad337841fb40a98294c7
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-06-10 20:05:50 -0500

    ionic start

---
+++ www/js/app.js
@@ -0,0 +1,12 @@
+angular.module('App', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
